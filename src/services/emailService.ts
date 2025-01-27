b908d7e1a86081f2e82feb2eb3aa99ce8588f667
yagosenhorini <yagosenhorini30@gmail.com>
2025-01-26 22:27:01 -0300

    fix(vulnerabilities): remove credentials

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -4,18 +4,18 @@ export const sendEmail = async (to: string, subject: string, body: string) => {
 	try {
 		// Configurar o transporte
 		const transporter = nodemailer.createTransport({
-			host: process.env.SMTP_HOST || "smtp.gmail.com", // Exemplo: Gmail
-			port: Number(process.env.SMTP_PORT) || 587,
+			host: process.env.SMTP_HOST, // Exemplo: Gmail
+			port: Number(process.env.SMTP_PORT),
 			secure: false, // true para 465, false para outras portas
 			auth: {
-				user: process.env.SMTP_USER || "yagosenhorini@gmail.com", // Seu email
-				pass: process.env.SMTP_PASS || "hnyq bxdb iaiu mkuc", // Sua senha ou app password
+				user: process.env.SMTP_USER, // Seu email
+				pass: process.env.SMTP_PASS, // Sua senha ou app password
 			},
 		});
 
 		// Opções do e-mail
 		const mailOptions = {
-			from: process.env.SMTP_FROM || '"Yago Senhorini" <yagosenhorini@gmail.com>', // Remetente
+			from: process.env.SMTP_FROM, // Remetente
 			to, // Destinatário
 			subject, // Assunto
 			text: body, // Corpo do email em texto simples
